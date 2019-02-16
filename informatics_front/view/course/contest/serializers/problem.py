12c656a54f371b667027de742f1f3ff3e7bfbfc4
n.pakhtusov <n.pakhtusov@tinkoff.ru>
2019-02-16 19:18:31 +0300

    fix (NFRMTCS-69): Fix problem serializer for working with sample tests json

--- informatics_front/view/course/contest/serializers/problem.py
+++ informatics_front/view/course/contest/serializers/problem.py
@@ -1,5 +1,7 @@
 from marshmallow import fields, Schema
 
+from informatics_front.model import Problem
+
 
 class ProblemSchema(Schema):
     id = fields.Integer(dump_only=True)
@@ -8,5 +10,9 @@ class ProblemSchema(Schema):
     timelimit = fields.Integer(dump_only=True)
     memorylimit = fields.Integer(dump_only=True)
     description = fields.String(dump_only=True)
-    sample_tests_json = fields.String(dump_only=True)
+    sample_tests_json = fields.Method(serialize='serialize_sample_tests')
     output_only = fields.Boolean(dump_only=True)
+
+    def serialize_sample_tests(self, obj: Problem):
+        obj.generateSamplesJson()
+        return obj.sample_tests_json
