b3ac9669a850013c672d1f678739e7853a208b6a
n.pakhtusov <n.pakhtusov@tinkoff.ru>
2019-03-06 13:44:11 +0300

    feat (NFRMTCS-110, NFRMTCS-112): F-strings in service urls

--- informatics_front/utils/services/internal_rmatics.py
+++ informatics_front/utils/services/internal_rmatics.py
@@ -23,7 +23,7 @@ class InternalRmatics(BaseService):
             'user_id': user_id,
             'statement_id': statement_id
         }
-        url = self.service_url + f'/problem/trusted/{problem_id}/submit_v2'
+        url = f'{self.service_url}/problem/trusted/{problem_id}/submit_v2'
 
         return self.client.post_data(url, json=data, files={'file': file.stream}, silent=True)
 
@@ -34,12 +34,12 @@ class InternalRmatics(BaseService):
             **args,
             'is_admin': is_admin,
         }
-        url = self.service_url + f'/problem/{problem_id}/submissions/'
+        url = f'{self.service_url}/problem/{problem_id}/submissions/'
 
         return self.client.get_data(url, params=filter_args, silent=True, default=[])
 
     def get_run_source(self, run_id: int, user_id, is_admin: bool = False) -> Tuple[dict, int]:
-        url = self.service_url + f'problem/run/{run_id}/source'
+        url = f'{self.service_url}problem/run/{run_id}/source'
 
         user_args = {
             'user_id': user_id,
@@ -49,7 +49,7 @@ class InternalRmatics(BaseService):
         return self.client.get_data(url, params=user_args, silent=True)
 
     def get_full_run_protocol(self, run_id: int, user_id: int, is_admin: bool = False) -> Tuple[dict, int]:
-        url = self.service_url + f'problem/run/{run_id}/source'
+        url = f'{self.service_url}problem/run/{run_id}/source'
 
         user_args = {
             'user_id': user_id,
