2c52ffb7384fb2bcc5c8750bbe80f18dac2434e5
Ilya Rusnen <ilya@rusanen.co.uk>
2019-07-15 12:36:11 +0300

    fix (NFRMTCS-273): Simplify generator-based search in array

--- workshop_admin/main/templatetags/workshop_extras.py
+++ workshop_admin/main/templatetags/workshop_extras.py
@@ -12,4 +12,5 @@ def public_link(workshop):
 
 @register.filter(name='humazine_wsconnection_status')
 def humazine_wsconnection_status(status):
-    return next((wc_status for wc_status in WORKSHOP_CONNECTION_STATUS_CHOICES if wc_status[0] == status), None)
+    a = [wc_status for wc_status in WORKSHOP_CONNECTION_STATUS_CHOICES if wc_status[0] == status]
+    return a and a[0] or None
